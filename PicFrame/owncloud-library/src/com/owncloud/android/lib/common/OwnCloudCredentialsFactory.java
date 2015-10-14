3037b5a7a8c1d4339f9452f1376458c1f9906320
Linda Spindler <if13b076@technikum-wien.at>
2015-10-14 13:19:40 +0200

    adding edited owncloud-library

---
+++ PicFrame/owncloud-library/src/com/owncloud/android/lib/common/OwnCloudCredentialsFactory.java
@@ -0,0 +1,78 @@
+/* ownCloud Android Library is available under MIT license
+ *   Copyright (C) 2015 ownCloud Inc.
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ */
+
+package com.owncloud.android.lib.common;
+
+public class OwnCloudCredentialsFactory {
+
+	private static OwnCloudAnonymousCredentials sAnonymousCredentials;
+
+	public static OwnCloudCredentials newBasicCredentials(String username, String password) {
+		return new OwnCloudBasicCredentials(username, password);
+	}
+	
+	public static OwnCloudCredentials newBearerCredentials(String authToken) {
+        return new OwnCloudBearerCredentials(authToken);
+	}
+    
+	public static OwnCloudCredentials newSamlSsoCredentials(String sessionCookie) {
+		return new OwnCloudSamlSsoCredentials(sessionCookie);
+	}
+
+	public static final OwnCloudCredentials getAnonymousCredentials() {
+		if (sAnonymousCredentials == null) {
+			sAnonymousCredentials = new OwnCloudAnonymousCredentials();
+		}
+		return sAnonymousCredentials;
+	}
+
+	public static final class OwnCloudAnonymousCredentials implements OwnCloudCredentials {
+		
+		protected OwnCloudAnonymousCredentials() {
+		}
+		
+		@Override
+		public void applyTo(OwnCloudClient client) {
+			client.getState().clearCredentials();
+			client.getState().clearCookies();
+		}
+
+		@Override
+		public String getAuthToken() {
+			return "";
+		}
+
+		@Override
+		public boolean authTokenExpires() {
+			return false;
+		}
+
+		@Override
+		public String getUsername() {
+			// no user name
+			return null;
+		}
+	}
+
+}
---
+++ PicFrame/owncloud-library/src/com/owncloud/android/lib/common/network/WebdavUtils.java
@@ -0,0 +1,134 @@
+/* ownCloud Android Library is available under MIT license
+ *   Copyright (C) 2015 ownCloud Inc.
+ *   Copyright (C) 2012 Bartek Przybylski
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ */
+
+package com.owncloud.android.lib.common.network;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.net.Uri;
+
+import org.apache.jackrabbit.webdav.property.DavPropertyName;
+import org.apache.jackrabbit.webdav.property.DavPropertyNameSet;
+import org.apache.jackrabbit.webdav.xml.Namespace;
+
+public class WebdavUtils {
+    public static final SimpleDateFormat DISPLAY_DATE_FORMAT = new SimpleDateFormat(
+            "dd.MM.yyyy hh:mm");
+    
+    private static final SimpleDateFormat DATETIME_FORMATS[] = {
+    	new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US),
+    	new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US),	
+		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", Locale.US),
+		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US),
+		new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US),
+		new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US),
+		new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US),
+		new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.US)
+	};
+
+    public static Date parseResponseDate(String date) {
+        Date returnDate = null;
+        SimpleDateFormat format = null;
+        for (int i = 0; i < DATETIME_FORMATS.length; ++i) {
+            try {
+            	format = DATETIME_FORMATS[i];
+            	synchronized(format) {
+            		returnDate = format.parse(date);
+            	}
+                return returnDate;
+            } catch (ParseException e) {
+            	// this is not the format
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Encodes a path according to URI RFC 2396. 
+     * 
+     * If the received path doesn't start with "/", the method adds it.
+     * 
+     * @param remoteFilePath    Path
+     * @return                  Encoded path according to RFC 2396, always starting with "/"
+     */
+    public static String encodePath(String remoteFilePath) {
+        String encodedPath = Uri.encode(remoteFilePath, "/");
+        if (!encodedPath.startsWith("/"))
+            encodedPath = "/" + encodedPath;
+        return encodedPath;
+    }
+
+    /**
+     * Builds a DavPropertyNameSet with all prop
+     * For using instead of DavConstants.PROPFIND_ALL_PROP
+     * @return
+     */
+    public static DavPropertyNameSet getAllPropSet(){
+        DavPropertyNameSet propSet = new DavPropertyNameSet();
+        propSet.add(DavPropertyName.DISPLAYNAME);
+        propSet.add(DavPropertyName.GETCONTENTTYPE);
+        propSet.add(DavPropertyName.RESOURCETYPE);
+        propSet.add(DavPropertyName.GETCONTENTLENGTH);
+        propSet.add(DavPropertyName.GETLASTMODIFIED);
+        propSet.add(DavPropertyName.CREATIONDATE);
+        propSet.add(DavPropertyName.GETETAG);
+        propSet.add(DavPropertyName.create(WebdavEntry.PROPERTY_QUOTA_USED_BYTES));
+        propSet.add(DavPropertyName.create(WebdavEntry.PROPERTY_QUOTA_AVAILABLE_BYTES));
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_PERMISSIONS,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_REMOTE_ID,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_SIZE,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+
+        return propSet;
+    }
+
+    /**
+     * Builds a DavPropertyNameSet with properties for files
+     * @return
+     */
+    public static DavPropertyNameSet getFilePropSet(){
+        DavPropertyNameSet propSet = new DavPropertyNameSet();
+        propSet.add(DavPropertyName.DISPLAYNAME);
+        propSet.add(DavPropertyName.GETCONTENTTYPE);
+        propSet.add(DavPropertyName.RESOURCETYPE);
+        propSet.add(DavPropertyName.GETCONTENTLENGTH);
+        propSet.add(DavPropertyName.GETLASTMODIFIED);
+        propSet.add(DavPropertyName.CREATIONDATE);
+        propSet.add(DavPropertyName.GETETAG);
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_PERMISSIONS,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_REMOTE_ID,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+        propSet.add(WebdavEntry.EXTENDED_PROPERTY_NAME_SIZE,
+                Namespace.getNamespace(WebdavEntry.NAMESPACE_OC));
+
+        return propSet;
+    }
+}
