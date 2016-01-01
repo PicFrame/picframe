3a696bdacf98fda57a9031d3f27fcf557bd650d6
hclemens <clemens1989@hotmail.com>
2016-01-01 23:12:10 +0100

    add Failure enums to Downloader.java

--- PicFrame/app/src/main/java/picframe/at/picframe/downloader/Downloader.java
+++ PicFrame/app/src/main/java/picframe/at/picframe/downloader/Downloader.java
@@ -4,4 +4,15 @@ import picframe.at.picframe.service_broadcast.ServiceCallbacks;
 
 public abstract class Downloader extends Thread {
     protected ServiceCallbacks serviceCallbacks;
+    public enum Failure {
+        LOGIN, WIFI, INTERRUPT, TIMEOUT, STORAGESPACE;
+        private static Failure[] allValues = values();
+        public static Failure getStatesForInt(int num) {
+            try{
+                return allValues[num];
+            } catch(ArrayIndexOutOfBoundsException e) {
+                return LOGIN;
+            }
+        }
+    }
 }
