bf123b2d23ecc2040d4f6a190d044cff8465cd59
Linda Spindler <if13b076@technikum-wien.at>
2016-01-17 18:01:33 +0100

    add link to about page, string translations

--- PicFrame/app/src/main/java/picframe/at/picframe/activities/StatusActivity.java
+++ PicFrame/app/src/main/java/picframe/at/picframe/activities/StatusActivity.java
@@ -1,18 +1,26 @@
 package picframe.at.picframe.activities;
 
 import android.app.AlarmManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.GregorianCalendar;
+import java.util.logging.Handler;
 
 import picframe.at.picframe.R;
 import picframe.at.picframe.helper.GlobalPhoneFuncs;
 import picframe.at.picframe.helper.alarm.AlarmScheduler;
 import picframe.at.picframe.helper.alarm.TimeConverter;
+import picframe.at.picframe.service.connectionChecker.ConnectionCheck_OC;
+import picframe.at.picframe.service.folderChecker.FolderReaderService;
+import picframe.at.picframe.service.folderChecker.RemoteFolderListDownloader_OC;
 import picframe.at.picframe.settings.AppData;
 
 public class StatusActivity extends ActionBarActivity {
@@ -32,8 +40,18 @@ public class StatusActivity extends ActionBarActivity {
         nbFiles = (TextView) findViewById(R.id.status_nbFiles);
         currentFolder = (TextView) findViewById(R.id.status_currentFolder);
         nextDownload = (TextView) findViewById(R.id.statusOC_nextAlarm);
-        nbRemoteOCFiles= (TextView) findViewById(R.id.statusOC_nbRomoteFiles);
+//        nbRemoteOCFiles= (TextView) findViewById(R.id.statusOC_nbRomoteFiles);
         lastLoginCheck = (TextView) findViewById(R.id.statusOC_loginCheck);
+        aboutButton = (Button) findViewById(R.id.status_buttonAbout);
+        aboutButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     protected void onResume(){
@@ -57,23 +75,26 @@ public class StatusActivity extends ActionBarActivity {
     }
 
     private void setOwnCloudStatus(){
-        String remoteOCFolder = "";
-        String remoteOCFileCount = "-";
+//        String remoteOCFolder = "";
+//        String remoteOCFileCount = "-";
         String nextDownload = getString(R.string.status_downloadIntervalNoDownload);
         String loginCheckResult = "-";
 
         if((AppData.getSourceType() == AppData.sourceTypes.OwnCloud) ) {
 
             Long nextAlarm = AppData.getNextAlarmTime();
-            if (nextAlarm != -1 && new GregorianCalendar().getTimeInMillis() >= nextAlarm) {
+            Log.d("nextAlarm", String.valueOf(nextAlarm));
+            if (nextAlarm != -1 && nextAlarm > new GregorianCalendar().getTimeInMillis()) {
                 TimeConverter tc = new TimeConverter();
                 nextDownload = tc.millisecondsToDate(nextAlarm);
             }
 
             loginCheckResult = (AppData.getLoginSuccessful()) ? "Successful" : "Failed";
+//            android.os.Handler handler = new android.os.Handler();
+//            handler.post(new RemoteFolderListDownloader_OC(handler))
         }
         this.nextDownload.setText(nextDownload);
-        this.nbRemoteOCFiles.setText(remoteOCFileCount);
+//        this.nbRemoteOCFiles.setText(remoteOCFileCount);
         this.lastLoginCheck.setText(loginCheckResult);
     }
 
